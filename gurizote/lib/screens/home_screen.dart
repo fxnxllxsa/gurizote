c89b7370b5c340825fb8028d34fb47604830244a
fxnxllxsa <hproductions393@gmail.com>
2025-03-18 20:38:33 -0300

    Drawer

--- gurizote/lib/screens/home_screen.dart
+++ gurizote/lib/screens/home_screen.dart
@@ -52,34 +52,17 @@ class _MyHomePageState extends State<MyHomePage> {
       
       ),
      drawer: Drawer(
+      child: Container(
+        color: Color.fromARGB(255, 156, 68, 207),
       child: ListView(
-        padding: EdgeInsets.all(20),
-        children: [Text('Corno')],
-     
-     
-     
-     
+        children: [
+        DrawerHeader(
+          child: Text('LOGO')
+        ,)
+        ],
       ),
-     ),
-      body: Center(
-   
-        child: Column(
-  
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      )
+     )
     );
-  }
+    }
 }
